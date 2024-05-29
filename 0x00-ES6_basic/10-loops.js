c9bb8dc2c99cfcc17856b1f6256fbed8d07dce8c
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2024-05-29 16:49:40 +0100

    Refactor appendToEachArrayValue function to use for...of loop

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,17 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx in array) {
-    const value = array[idx];
-    array[idx] = appendString + value;
-  }
-
-  return array;
-}
-
-export default function appendToEachArrayValue(array, appendString) {
   // eslint-disable-next-line
-	for (const value of array) {
+  for (const value of array) {
     // eslint-disable-next-line
-		array[array.indexOf(value)] = appendString + value;
+    array[array.indexOf(value)] = appendString + value;
   }
 
   return array;
